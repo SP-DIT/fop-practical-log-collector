cc2335eea191d831cf11f0f2974191c74c75690d
Jeremiah Ang <jeremiahang94@gmail.com>
2025-04-19 15:28:13 +0000

    Add cors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import validate from './schema.js';
 import logger from './logger.js';
 import createHttpError from 'http-errors';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 // Middleware to parse JSON requests
 app.use(express.json());
