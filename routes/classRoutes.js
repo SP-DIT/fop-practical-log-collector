4fee4c45eb74d0b9f52c42fb97b83e24a8d5b4d8
Su-Yee-Phyo <SUYEE.23@ichat.sp.edu.sg>
2025-05-15 16:25:27 +0800

    joining the user and points table first

--- routes/classRoutes.js
+++ routes/classRoutes.js
@@ -1,9 +1,9 @@
 // routes/classRoutes.js
 import express from 'express';
-import { getAllClass } from '../controller/classController.js';
+import { getAllClass, getAllClassRank } from '../controller/classController.js';
 
 const router = express.Router();
 
 router.get('/', getAllClass);
-
+router.get('/rank', getAllClassRank);
 export default router;
